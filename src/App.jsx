86a792f7eea6bf1c7cff28f5866da4eb9eb41e72
Ravi Kant <kumarra927@gmail.com>
2025-07-13 20:44:53 +0530

    frame

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Main from "./components/Main"
 
-
 function App() {
 
   return (
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>
-      
+    <div className='bg-[#f3f4f6] flex justify-center w-full h-screen'>
+      <div className='bg-white flex flex-col items-center py-5 w-[350px] my-6 rounded-xl shadow-xl'>
+        <h1 className='font-extrabold text-xl'>Split<span className='text-[#0800ff]'>Cards</span></h1>
+        <div></div>
+        <div className=''>
+           <img src=''/>
+        </div>
+      </div>
     </div>
   )
 }
